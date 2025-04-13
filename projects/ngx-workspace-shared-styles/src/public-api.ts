7e349650185a7156830b2d300127b2495bcbdde9
XOK <a.xandr.q@gmail.com>
2025-04-13 03:08:22 +0200

    add ngx shared styles variables

--- projects/ngx-workspace-shared-styles/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of ngx-workspace-shared-styles
- */
-
-export * from './lib/ngx-workspace-shared-styles.service';
-export * from './lib/ngx-workspace-shared-styles.component';
